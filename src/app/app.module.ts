7e004c46ff3df08f1c7bb12409f3acffe6b4906e
Vladyslav Havryliuk <vhavryliuk@corevalue.net>
2019-11-26 13:28:34 +0200

    add @ngrx/store-devtools module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {StoreModule} from '@ngrx/store';
 import {reducers, metaReducers} from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
     declarations: [
@@ -19,7 +21,8 @@ import {reducers, metaReducers} from './reducers';
                 strictStateImmutability: true,
                 strictActionImmutability: true
             }
-        })
+        }),
+        StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
     ],
     providers: [],
     bootstrap: [AppComponent]
