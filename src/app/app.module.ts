76ed9760d4703a3d1d4a510fcf487639c758f45c
Vladyslav Havryliuk <vhavryliuk@corevalue.net>
2019-11-28 17:03:17 +0200

    make store directory and move to its all custom files of @ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {StoreModule} from '@ngrx/store';
-import {reducers, metaReducers} from './reducers';
+import {reducers, metaReducers} from './store/reducers';
 import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 import {environment} from '../environments/environment';
 import {EffectsModule} from '@ngrx/effects';
-import {AppEffects} from './effects/app.effects';
+import {AppEffects} from './store/effects/app.effects';
 
 @NgModule({
     declarations: [
